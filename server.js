e0dc85329f16e844d838603d163487e7e6c8a96b
Avisa-SEI <aap571420@gmail.com>
2021-06-10 13:54:57 -0700

    add: user model. users controllers session view and controllers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 // Database Configuration
 mongoose.connect(process.env.DATABASE_URL, {
@@ -18,6 +19,31 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+// MIDDLEWARE
+// BODY PARSER MIDDLEWARE: GIVE US ACCESS TO REQ.BODY
+app.use(express.urlencoded({ extended: true }))
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
+
+// ROUTES / CONTROLLERS
+const userController = require('./controllers/users.js')
+app.use('/users', userController)
+const sessionsController = require('./controllers/sessions');
+app.use('/sessions', sessionsController);
+
+//  ROUTES
+// INDEX
+// Routes / Controllers
+app.get('/', (req, res) => {
+	res.render('index.ejs', {
+        currentUser: req.session.currentUser
+    });
+});
 
 // Listener
 const PORT = process.env.PORT;
